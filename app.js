b9c08c80e525314b22300231185f27c524b71d38
mndowne <mndowne@gmail.com>
2016-10-27 23:21:09 -0500

    added index.html

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ var os = require('os');
 
 app.set('port',(process.env.PORT || 8080));
 
-app.get('/', function(req,res){
+app.use(express.static(__dirname));
 
-res.send(req.connection.remoteAddress + ":::" + process.platform + "\n"
+
+app.get('/here', function(req,res){
+
+res.send(req.headers['x-forwarded-for']+ ":::" + process.platform + "\n"
  + os.platform() + os.release());
 
-
 });
+
+
 
 //app.listen(3000, function() {console.log('server is up on 3000 or process env');}); 
 app.listen(app.get('port'), function() {console.log('server is up on 8080 or process env');}); 
